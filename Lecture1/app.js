dbfe857a12d9852666e820cd65d8c42f3de8acf1
anshul yadav <anshulgamers60@gmail.com>
2026-07-05 12:37:17 +0530

    first commit typecript

---
+++ Lecture1/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const a = 20;
+const b = 30;
+console.log(add(a, b));
+function add(a, b) {
+    return a + b;
+}
---
+++ Lecture1/app.ts
@@ -0,0 +1,8 @@
+const a = 20;
+const b = 30;
+
+console.log(add(a, b));
+
+function add(a: number, b: number) {
+  return a + b;
+}
